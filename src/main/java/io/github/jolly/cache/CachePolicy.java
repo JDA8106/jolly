bacc2beff89b52e0c165036e4b4a0e0bd0379320
anish <anishvisaria98@gmail.com>
2018-10-17 23:58:22 -0400

    CachePolicy implementation

--- src/main/java/io/github/jolly/cache/CachePolicy.java
+++ src/main/java/io/github/jolly/cache/CachePolicy.java
@@ -1,4 +1,43 @@
 package io.github.jolly.cache;
 
-public class CachePolicy {
+import io.github.resilience4j.cache.Cache;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Cache Policy for Jolly
+ *
+ * @author Anish Visaria
+ *
+ * @param <K> Input of Cached Function
+ * @param <V> Output of Cached Function
+ *
+ */
+public class CachePolicy<K, V> {
+
+    private Cache<K, V> cacheContext;
+    private Function<K, V> cachedFunction;
+
+    /**
+     * Instantiates Cache Policy with function to cache.
+     * @param function function to cache
+     */
+    public CachePolicy(Supplier<V> function) {
+        javax.cache.Cache<K, V> cacheInstance = CacheManager.getInstance().getCache("policy");
+        cacheContext = Cache.of(cacheInstance);
+        cachedFunction = Cache.decorateSupplier(cacheContext, function);
+    }
+
+
+    /**
+     * Fetches result from cache or from the function, if unavailable.
+     * @param cacheKey input of function
+     * @return Output of function for particular input
+     */
+    public V getResult(K cacheKey) {
+        return cachedFunction.apply(cacheKey);
+    }
+
+
 }
